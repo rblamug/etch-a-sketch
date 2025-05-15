46c1500183d5bdc6fa2d68e2acd3568391df0a81
Romel <200869522+rblamug@users.noreply.github.com>
2025-05-15 19:23:46 -0400

    added a "mouseover" event listener
    can now "draw" in black

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,7 +2,13 @@ const container = document.querySelector('#container');
 
 for (let i = 1; i <=256; i++) {
     const newDiv = document.createElement("div");
-    newDiv.textContent = `Div ${i}`;
+    newDiv.className = "grid";
     container.appendChild(newDiv);
 }
 
+const sketch = document.querySelectorAll('.grid');
+sketch.forEach(sketch => {
+    sketch.addEventListener("mouseenter", (e) => {
+        e.target.style.backgroundColor = "black";
+    })
+})
