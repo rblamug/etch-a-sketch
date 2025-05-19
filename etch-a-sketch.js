932ed96b1e8028b63633e1ad1a5a27af87f0450c
Romel <200869522+rblamug@users.noreply.github.com>
2025-05-19 19:20:44 -0400

    added rules for canvas size, button is functional
    it does become unresponsive or crashes with anyth-
    ng above 50 squares per side. figure this is nor-
    mal

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('#container');
+    const container = document.querySelector('#container');
 
 for (let i = 1; i <=256; i++) {
     const newDiv = document.createElement("div");
@@ -16,5 +16,34 @@ sketch.forEach(sketch => {
 // Button click to ask canvas size
 const sizer = document.querySelector('button')
 sizer.addEventListener("click", () => {
-    const size = prompt("Enter number of squares per side:");
+    const userInput = prompt("Enter number of squares per side (1-100):",);
+    const size = Number(userInput);
+    if (isNaN(size) || size < 1 || size > 100) {
+        console.log('User input in not a number, retrigger event');
+        alert("Please enter a number between 1 - 100");
+        sizer.dispatchEvent(new Event("click"));
+    } else {
+        console.log(`The user entered ${size}`);
+        container.textContent = '';
+        const newHeightAndWidth = 1024 / size;
+        console.log(`New height and width is ${newHeightAndWidth}`);
+        const totalBoxes = size * size;
+
+        for (i = 1; i < totalBoxes; i++) {
+            const newDiv = document.createElement("div");
+            newDiv.className = "grid";
+            newDiv.style.height = `${newHeightAndWidth}px`;
+            newDiv.style.width = `${newHeightAndWidth}px`;
+            container.appendChild(newDiv);
+
+            //retrigger the eventlistener so the user can draw again
+            const sketch = document.querySelectorAll('.grid');
+            sketch.forEach(sketch => {
+            sketch.addEventListener("mouseenter", (e) => {
+            e.target.style.backgroundColor = "black";
+    })
 })
+
+        };
+    };
+});
